9921a38f181a1878c156164d0a0b9155d1a60e59
Martin Vala <mvala@saske.sk>
2016-05-24 15:21:19 +0200

    Added make script

---
+++ scripts/make.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+HK_DIR="$(dirname $(dirname $(readlink -m $0)))"
+
+[ "$1" = "clean" ] && [ -d $HK_DIR/build ] && rm -rf $HK_DIR/build && mkdir $HK_DIR/build
+
+cd $HK_DIR/build || exit 1
+cmake -DCMAKE_INSTALL_PREFIX=$HK_DIR -DCMAKE_BUILD_TYPE=RelWithDebInfo ../
+make -j$(nproc) install
+
