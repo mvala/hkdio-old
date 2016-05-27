f55d1cf38961f5dee6f395288fee2b3ad079d9b4
Martin Vala <mvala@saske.sk>
2016-05-27 10:54:12 +0200

    Fix for build directory removing

--- scripts/make.sh
+++ scripts/make.sh
@@ -2,7 +2,8 @@
 
 HK_DIR="$(dirname $(dirname $(readlink -m $0)))"
 
-[ "$1" = "clean" ] && [ -d $HK_DIR/build ] && rm -rf $HK_DIR/build && mkdir $HK_DIR/build
+[ "$1" = "clean" ] && rm -rf $HK_DIR/build
+[ -d $HK_DIR/build ] ||  mkdir $HK_DIR/build
 
 cd $HK_DIR/build || exit 1
 cmake -DCMAKE_INSTALL_PREFIX=$HK_DIR -DCMAKE_BUILD_TYPE=RelWithDebInfo ../
