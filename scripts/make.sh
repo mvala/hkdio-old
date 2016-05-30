f089188b0b9b72fbec8b6e2d01016c68dfc9151c
Martin Vala <mvala@saske.sk>
2016-05-30 09:56:02 +0200

    Added Aliroot support to make.sh

--- scripts/make.sh
+++ scripts/make.sh
@@ -5,7 +5,9 @@ HK_DIR="$(dirname $(dirname $(readlink -m $0)))"
 [ "$1" = "clean" ] && rm -rf $HK_DIR/build
 [ -d $HK_DIR/build ] ||  mkdir $HK_DIR/build
 
+[ -n "$ALICE_ROOT" ] && HK_ALICE="-DALIROOT=$ALICE_ROOT"
+
 cd $HK_DIR/build || exit 1
-cmake -DCMAKE_INSTALL_PREFIX=$HK_DIR -DCMAKE_BUILD_TYPE=RelWithDebInfo ../
+cmake -DCMAKE_INSTALL_PREFIX=$HK_DIR -DCMAKE_BUILD_TYPE=RelWithDebInfo $HK_ALICE ../
 make -j$(nproc) install
 
